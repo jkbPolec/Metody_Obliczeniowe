50e35e000d4f05e46790c6b126b1ae5529240c93
Jakub Połeć <jkb.polec@gmail.com>
2025-03-01 15:10:33 +0100

    Dodałem opis zadania i funkcje do liczenia liczby znaczacych cyfr epsilona

--- main.c
+++ main.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include <float.h>
+#include <math.h>
+
+/*  Temat zadania:
+Napisz program w języku „C/C++”, umożliwiający „doświadczalne”
+wyznaczenie liczby bitów mantysy oraz tzw. epsilon maszynowego,
+tj. najmniejszej liczby ε takiej, że fl(ε + 1) > 1.
+Wyznacz epsilon maszynowy dla zmiennych typu float i double i
+sprawdź czy da się go wyznaczyć dla zmiennych typu long double.
+Sprawdź też ile dokładnych cyfr znaczących posiada epsilon maszynowy.
+Aby znaleźć odpowiedź na pytanie jak napisać taki program,
+zacznij od wyjaśnienia kwestii jaki jest związek ε z precyzją arytmetyki.
+ */
 
 void epsFloat(float prec);
 void epsDouble(double prec);
 void epsLongDouble(long double prec);
-
+int significantDigits(double epsilon);
 
 int main() {
     epsFloat(1.0f);
@@ -32,6 +44,7 @@ void epsFloat(float prec) {
     ep = 2.0f * ep;
 
     printf("Float epsilon: %.65f\nFloat mantiss: %d\n", ep, mantissa);
+    printf("Float significant digits: %d\n", significantDigits(ep));
 }
 
 void epsDouble(double prec) {
@@ -45,6 +58,7 @@ void epsDouble(double prec) {
     ep = 2.0 * ep;
 
     printf("Double epsilon: %.65lf\nDouble mantiss: %d\n", ep, mantissa);
+    printf("Double significant digits: %d\n", significantDigits(ep));
 }
 
 void epsLongDouble(long double prec) {
@@ -58,4 +72,10 @@ void epsLongDouble(long double prec) {
     ep = 2.0L * ep;
 
     printf("Long double epsilon: %.65Lf\nLong double mantiss: %d\n", ep, mantissa);
+    printf("Long double significant digits: %d\n", significantDigits(ep));
+
+}
+
+int significantDigits(double epsilon) {
+    return (int)(-log10(epsilon));
 }
