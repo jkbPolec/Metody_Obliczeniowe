1010277d2cd93e806b84c3d3d75d404a4c67c826
Jakub Połeć <jkb.polec@gmail.com>
2025-03-01 12:54:51 +0100

    Wyczyszczono maina

--- main.c
+++ main.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
-#include <float.h>
-
-void epsFloat(float prec);
-void epsDouble(double prec);
-void epsLongDouble(long double prec);
-
 
 int main() {
 
     printf("Witam w repo do Metod obliczeniowych\n");
-
-
+    printf("Aby zobaczyc zadania z innych zajec, zmien branch\n");
     return 0;
 }
